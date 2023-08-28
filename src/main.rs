0369dbac09cff133e8a937c9baa2add895c74d35
Petros Giouroukis <pgiouroukis@gmail.com>
2023-08-28 12:17:27 +0300

    Semantic: Init - Check for re-declaration of variables
    In this commit, we start working on the semantic analyzer.
    
    In order to perform semantic checks, the semantic analyzer requires an AST.
    
    We implement our first semantic check, which checks for re-declaration of variables.
    
    We also populate the symbol table.

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,38 @@ mod parser;
 mod lexer;
 mod utilities;
 mod mixal;
+mod semantic_analyzer;
 
 use crate::{utilities::get_tokens_from_program, mixal::assembler::MixalAssembler};
 use crate::parser::Parser;
+use crate::semantic_analyzer::SemanticAnalyzer;
 
 fn main() {
 
     let program: String = String::from(
         "{ \
-            var alpha, i: int; \
-            alpha = -1; \
-            for (i = 0; i < 4; i+=1) \
-                alpha *= 2; \
-            print alpha; \
-            print alpha * 2 + 3; \
+            var i, j, i: int; \
+            var i: int; \
+            i = 1;
+            j = 2;
         }",
     );
 
     let tokens = get_tokens_from_program(&program);
     let mut parser = Parser::new(tokens);
-    
     if parser.analyze_grammar() {
         println!("Parsing successful");
     } else {
         println!("Parsing failed");
+        return;
+    }
+
+    let mut semantic_checker = SemanticAnalyzer::new(&parser.ast);
+    if semantic_checker.run() {
+        println!("All semantic checks passed");
+    } else {
+        println!("Some semantic checks failed");
+        return;
     }
 
     let mut assembler = MixalAssembler::new(
---
+++ src/semantic_analyzer.rs
@@ -0,0 +1,42 @@
+use orange_trees::Node;
+use crate::lexer::Token;
+use std::collections::HashSet;
+
+pub struct SemanticAnalyzer<'a> {
+    pub ast: &'a Node<usize, Token>,
+    pub symbol_table: HashSet<&'a String>
+}
+
+impl<'a> SemanticAnalyzer<'a> {
+    pub fn new(ast: &Node<usize, Token>) -> SemanticAnalyzer {
+        SemanticAnalyzer {
+            ast,
+            symbol_table: HashSet::new()
+        }
+    }
+
+    pub fn run(&mut self) -> bool {
+        let mut violations = 0;
+        violations += self.populate_symbol_table_and_check_for_variable_re_declarations();
+        return violations == 0;
+    }
+
+    fn populate_symbol_table_and_check_for_variable_re_declarations(&mut self) -> u8 {
+        let mut violations = 0;
+        let variable_declaration_nodes = self.ast.find(&|x| *x.value() == Token::Int);
+        for variable_declaration_node in variable_declaration_nodes {
+            for identifier_node in variable_declaration_node.children() {
+                if let Token::Id(identifier_name) = identifier_node.value() {
+                    if self.symbol_table.contains(&identifier_name) {
+                        println!("ERROR: re-declaration of identifier '{}'", &identifier_name);
+                        violations += 1;
+                    } else {
+                        self.symbol_table.insert(identifier_name);
+                    }
+                    
+                }
+            }
+        }
+        return violations;
+    }
+}
